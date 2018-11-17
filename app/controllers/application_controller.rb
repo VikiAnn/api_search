adc0e4ba27ca952114bbe03c7311656edf279cca
Viki Harrod <victoria.harrod@gmail.com>
2018-11-16 18:23:02 -0700

    Add navigation helper, move nav link out of the main flexbox

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :navigation
+
+  def navigation
+    if request.path == root_path
+      ["Search History", search_history_path]
+    else
+      ["Home", root_path]
+    end
+  end
 end
